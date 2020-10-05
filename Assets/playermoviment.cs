aa4b92f486cbebc0168116f6e561532adaf1bde7
DeDar4p <gabrieliftm8@gmail.com>
2020-10-05 03:27:20 -0300

    gravidade e pulo

--- Assets/playermoviment.cs
+++ Assets/playermoviment.cs
@@ -6,6 +6,13 @@ public class playermoviment : MonoBehaviour
 {
     public CharacterController controller;
     public float speed = 12f;
+    public float gravity = -9.81f;
+    public float jumpheigh = 3f;
+    Vector3 velocitity;
+    bool isgrounded;
+    public Transform GroundCheck;
+    public float groundDistance = 0.3f;
+    public LayerMask groundMask;
     // Start is called before the first frame update
     void Start()
     {
@@ -15,10 +22,26 @@ public class playermoviment : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        isgrounded = Physics.CheckSphere(GroundCheck.position, groundDistance, groundMask);
+        if(isgrounded && velocitity.y < 0)
+        {
+            velocitity.y = 0f;
+        }
         float x = Input.GetAxis("Horizontal");
         float z = Input.GetAxis("Vertical");
 
         Vector3 move = transform.right * x + transform.forward * z;
+      
         controller.Move(move * speed * Time.deltaTime);
+
+        if(Input.GetButtonDown("Jump") && isgrounded)
+        {
+            velocitity.y = Mathf.Sqrt(jumpheigh * -2 * gravity);
+        }
+
+        velocitity.y += gravity * Time.deltaTime;
+
+        controller.Move(velocitity * Time.deltaTime);
+
     }
 }
